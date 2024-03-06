ee6be1d4e54e179302ef886ad602b705676d11d4
LouisLU9911 <louislu9911@gmail.com>
2024-03-06 16:32:05 +0000

    fix: fix requiresAuth for dashboard

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -24,7 +24,6 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/LoginView.vue'),
-      meta: { requiresAuth: true },
     },
     {
       path: '/dashboard',
@@ -32,7 +31,8 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/DashboardView.vue')
+      component: () => import('../views/DashboardView.vue'),
+      meta: { requiresAuth: true },
     },
   ]
 })
